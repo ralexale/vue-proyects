70f83913ffe1669fa5829548e4bac878ef1d9909
ralexale <ralexale@gmail.com>
2024-07-16 09:42:01 -0500

    add router app with layouts

---
+++ 05-vue-spa/src/router/index.ts
@@ -0,0 +1,66 @@
+import AuthLayout from '@/modules/auth/layouts/AuthLayout.vue';
+import LandingLayout from '@/modules/lading/layouts/LandingLayout.vue';
+import HomePage from '@/modules/lading/pages/HomePage.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // landing
+    {
+      path: '/',
+
+      component: LandingLayout,
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomePage,
+        },
+        {
+          path: '/features',
+          name: 'features',
+          component: () => import('@/modules/lading/pages/FeaturesPage.vue'),
+        },
+        {
+          path: '/pricing',
+          name: 'pricing',
+          component: () => import('@/modules/lading/pages/PricingPage.vue'),
+        },
+        {
+          path: '/contact',
+          name: 'contact',
+          component: () => import('@/modules/lading/pages/ContactPage.vue'),
+        },
+      ],
+    },
+
+    // auth
+
+    {
+      path: '/auth',
+      redirect: 'auth/login',
+      component: AuthLayout,
+      children: [
+        {
+          path: 'login',
+          component: () => import('@/modules/auth/pages/LoginPage.vue'),
+          name: 'login',
+        },
+        {
+          path: 'register',
+          component: () => import('@/modules/auth/pages/RegisterPage.vue'),
+          name: 'register',
+        },
+      ],
+    },
+
+    // not found
+    {
+      path: '/:pathMatch(.*)*',
+      component: () => import('@/modules/common/pages/NotFound404.vue'),
+    },
+  ],
+});
+
+export default router;
